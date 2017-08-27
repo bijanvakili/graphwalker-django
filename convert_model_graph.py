aa6e192a09504254413b05de36d6d1d39f5b2733
Bijan Vakili <users.noreply.github.com>
2017-08-27 16:40:12 -0700

    Fixed bugs and restructured to new format

--- convert_model_graph.py
+++ convert_model_graph.py
@@ -3,10 +3,9 @@ This converts django_extensions graph_models JSON output to graphwalker JSON for
 """
 
 # TODO Support both python 2 and 3
-
+import hashlib
 import json
 import sys
-import traceback
 
 
 # maps Django type to multiplicity contains both forward and reverse versions
@@ -18,11 +17,14 @@ MULTIPLICITY_MAP = {
 }
 
 
-def _make_key(*components):
-    return reduce(
-        lambda key, component: key + (('#' + component) if key else component),
-        components
-    )
+def _make_vertex_id(qualified_model_name):
+    return hashlib.sha1(qualified_model_name).hexdigest()
+
+
+def _make_edge_id(source_vertex, dest_vertex, relation):
+    return hashlib.sha1(
+        '{}({},{})'.format(relation['type'], source_vertex['id'], dest_vertex['id'])
+    ).hexdigest()
 
 
 def convert(django_graph_data):
@@ -31,24 +33,31 @@ def convert(django_graph_data):
 
     for graph in django_graph_data['graphs']:
         for model in graph['models']:
+            qualified_model_name = '{}.{}'.format(graph['app_name'], model['name'])
             new_vertex = {
-                'internalAppName': model['app_name'],
-                'appName': graph['app_name'],
-                'modelName': model['name']
+                'id': _make_vertex_id(qualified_model_name),
+                'label': model['name'],
+                'searchableComponents': [graph['app_name'], model['name']],
+                'properties': {
+                    'internalAppName': model['app_name'],
+                    'appName': graph['app_name'],
+                    'modelName': model['name'],
+                    'baseClasses': model.get('abstracts', [])
+                }
             }
             vertices.append(new_vertex)
 
-            # store composite key references for both the app name and internal app name
-            vertex_map[_make_key(graph['app_name'], model['name'])] = new_vertex
-            vertex_map[_make_key(model['app_name'], model['name'])] = new_vertex
+            # store composite key references via tuples for both the app name and internal app name
+            vertex_map[(graph['app_name'], model['name'])] = new_vertex
+            vertex_map[(model['app_name'], model['name'])] = new_vertex
 
     edges = []
     edge_map = {}
     for graph in django_graph_data['graphs']:
         for model in graph['models']:
             for relation in model['relations']:
-                source_vertex = vertex_map.get(_make_key(graph['app_name'], model['name']))
-                dest_vertex = vertex_map.get(_make_key(relation['target_app'], relation['target']))
+                source_vertex = vertex_map.get((graph['app_name'], model['name']))
+                dest_vertex = vertex_map.get((relation['target_app'], relation['target']))
 
                 if source_vertex and dest_vertex:
                     multiplicity = MULTIPLICITY_MAP.get(relation['type'])
@@ -59,27 +68,36 @@ def convert(django_graph_data):
                         multiplicity = None
 
                     # deduplicate and merge where possible
-                    criteria = {
-                        'source': source_vertex,
-                        'dest': dest_vertex,
-                        'type': relation['type'],
-                        'label': None,
-                        'multiplicity': None
+                    new_edge = {
+                        'id': _make_edge_id(source_vertex, dest_vertex, relation),
+                        'source': source_vertex['id'],
+                        'dest': dest_vertex['id'],
+                        'properties': {
+                            'type': relation['type'],
+                            'multiplicity': multiplicity,
+                            'fields': [relation['name']]
+                        }
                     }
-                    edge_key = _make_key(
-                        criteria['source'].get('appName'),
-                        criteria['source'].get('modelName'),
-                        criteria['dest'].get('appName'),
-                        criteria['dest'].get('modelName'),
-                        criteria['type']
+                    edge_key = (
+                        source_vertex['id'],
+                        dest_vertex['id'],
+                        relation['type'],
                     )
                     existing_edge = edge_map.get(edge_key)
                     if not existing_edge:
-                        criteria['label'] = relation['name']
-                        criteria['multiplicity'] = multiplicity
-                        edge_map[edge_key] = edges.append(criteria)
+                        # the name of the model property/column or just 'inheritance'
+                        edges.append(new_edge)
+                        edge_map[edge_key] = new_edge
                     else:
-                        existing_edge['label']= existing_edge.get('label', '') + ', ' + relation.name
+                        # if the same edge already exists with a different label, we collapse the labels
+                        # into a comma separated list
+                        existing_edge['properties']['fields'].append(relation['name'])
+
+        # recompute label names
+        for edge in edges:
+            edge['label'] = ', '.join(edge['properties']['fields'])
+            if edge['properties'].get('multiplicity'):
+                edge['label'] += ' ({})'.format(edge['properties']['multiplicity'])
 
         return {
             'vertices': vertices,
